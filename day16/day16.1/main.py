a1dd73144eddd0cad8615727c694ec5a37415762
jamstanleyambe <jamstanleyambe@gmail.com>
2024-02-04 02:05:50 +0100

    new commit

---
+++ day16/day16.1/main.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color('coral')
+
+timmy.forward(200)
+timmy.left(320)
+timmy.right(300)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
