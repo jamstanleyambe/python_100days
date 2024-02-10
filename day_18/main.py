b710e3bf42847b89a434fc92e766ba5464de857a
jamstanleyambe <jamstanleyambe@gmail.com>
2024-02-11 00:09:20 +0100

    new commit

---
+++ day_18/main.py
@@ -0,0 +1,19 @@
+from turtle import Turtle, Screen
+
+tin = Turtle()
+tin.shape("turtle")
+tin.color("green")
+
+for i in  range(4):
+    tin.forward(100)
+    tin.right(90)
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
